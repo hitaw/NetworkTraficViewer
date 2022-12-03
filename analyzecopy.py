d87f9d88efc73f85ee8dd6b0ceadb922f362b4f6
Maina02 <maina.laurent3009@gmail.com>
2022-12-03 14:43:17 +0100

    solution pb IPv4

--- analyzecopy.py
+++ analyzecopy.py
@@ -112,31 +112,48 @@ class Trame:
 
 	def analyze_ipv4(self):
 		self.ip_v = int(self.non_etud[0],16)
-		if self.ip_v != 4:
-			return "error"
-		self.header_length = int(self.non_etud[1],16)
-		self.ToS =  int(self.non_etud[2:4],16)
-		self.total_length = int(self.non_etud[4:8],16)
-		self.identifier = int(self.non_etud[8:12],16)
 
-		temp = bin(int(self.non_etud[13],16))
-		self.ip_flags = temp[2:5]
-		self.fragment_offset = temp[6] + self.non_etud[14:17]
-		self.time_to_live = int(self.non_etud[17:19],16)
-		self.protocol = self.non_etud[19:21]
-		self.header_checksum = self.non_etud[21:25]
+		if self.ip_v == 4:
+			self.header_length = int(self.non_etud[1],16)
+			self.ToS =  int(self.non_etud[2:4],16)
+			self.total_length = int(self.non_etud[4:8],16)
+			self.identifier = int(self.non_etud[8:12],16)
 
-		src_ip = self.non_etud[25:33]
-		dest_ip = self.non_etud[33:41]
-		i = 0
-		while i < 8:
-			self.dest_ip += str(int(dest_ip[i:i+4],16))+":"
-			self.src_ip += str(int(src_ip[i:i+4],16))+":"
-			i += 4
+			temp = bin(int(self.non_etud[12],16))
+			temp_size = len(temp)
+			if temp_size < 5 :
+				while temp_size <= 5:
+					temp += "0"
+					temp_size +=1
 
-		options_size = self.header_length*4 - 20
-		i = 41 + options_size
-		self.non_etud = self.non_etud[i:]
+			self.ip_flags = temp[2:4]
+			self.fragment_offset = temp[4] + self.non_etud[13:16]
+
+			self.time_to_live = int(self.non_etud[16:18],16)
+			self.protocol = self.non_etud[18:20]
+			self.header_checksum = self.non_etud[20:24]
+
+			src_ip = self.non_etud[24:32]
+			dest_ip = self.non_etud[32:40]
+
+			i = 0
+			while i < 8:
+				self.dest_ip += str(int(dest_ip[i:i+2],16))+"."
+				self.src_ip += str(int(src_ip[i:i+2],16))+"."
+				i += 2
+
+			self.dest_ip = self.dest_ip[:-1]
+			self.src_ip = self.src_ip[:-1]
+
+			options_size = self.header_length*4 - 20
+			i = 40 + options_size
+
+			self.options_padding_ip = self.non_etud[40:i]
+
+			self.non_etud = self.non_etud[i:]
+
+		else:
+			self.mess_error = 'Mauvaise valeur : ip_version != 4'
 
 
 
