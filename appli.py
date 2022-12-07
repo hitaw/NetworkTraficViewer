5b4a63bb5ac63b847a3a3ed145becf7c744dad45
Maina02 <maina.laurent3009@gmail.com>
2022-12-07 10:42:39 +0100

    impression

--- appli.py
+++ appli.py
@@ -1,4 +1,6 @@
 from interface import *
+from impression import *
 
 window = Interface()
 window.mainloop()
+impression_pdf()
--- impression.py
+++ impression.py
@@ -1,13 +1,7 @@
-from win32gui import *
 from tkinter import *
+from interface import *
+from PIL import ImageGrab, Image
 
 def impression_pdf():
-	master = Tk()
-	cv = Canvas(master, width=200, height=100)
-	cv.pack()
-	cv.create_rectangle(50, 25, 150, 75, fill="blue")
-	l = Label(cv, text='Label', bd=0, padx=3, pady=1)
-	cv.create_window(20, 20, window=l)
-	b = Button(cv, text='draw', command=lambda :cv.postscript(file="ecran.ps", colormode='color'))
-	cv.create_window(100, 48, window=b)
-	mainloop()
+	img = ImageGrab.grab()
+	img.save('screen_fireshark.pdf',save_all=True)
